67431ee1fccaf2a5d980f9309f0d4e1f09c8afc0
Hayden <hayden.fuller@gmail.com>
2020-08-03 22:22:47 +1000

    most work finshed for new website, need to update cards

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+options = {
+    edge: 'right',
+    preventScrolling: true
+}
+
+
+
